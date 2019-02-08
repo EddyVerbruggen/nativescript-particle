81c08be40541b7d4a6d19be9bab13b4152a2a62c
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-02-08 14:03:44 +0100

    ci

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+
+/// <reference path="./platforms/ios/typings/objc!Particle_SDK-0.8.1.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!ParticleSetup-0.9.0.d.ts" />
+
+/// <reference path="./platforms/android/typings/cloudsdk-0.5.1.d.ts" />
+/// <reference path="./platforms/android/typings/devicesetup-0.6.0.d.ts" />
